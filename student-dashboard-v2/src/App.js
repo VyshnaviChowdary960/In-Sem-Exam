2150ad5e1049543f75d7da68b0a841d6e66f3450
unknown <Vyshnavichowdary960@gmail.com>
2025-10-15 19:05:23 +0530

    first commit

---
+++ student-dashboard-v2/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Welcome from './components/Welcome';
+
+
+function App() {
+  const students = ['Deepak', 'Amit', 'Sara'];
+  return (
+    <div>
+      {students.map(student => (
+        <Welcome key={student} name={student} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ student-dashboard-v2/src/components/welcome.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function Welcome({ name }) {
+  return <h2>Welcome, {name}!</h2>;
+}
+export default Welcome;
